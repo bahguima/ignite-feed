15b2a5bd2f8c35594b199d60d4b8d0d9e7309c5e
Bárbara Guieiro <barbaraguieiro@gmail.com>
2023-08-12 00:45:04 -0300

    Header and Sidebar css / initial components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Bárbara Guieiro"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
