8018d4df8cd48c5f716e1ac09d9e909f63b0ccd0
Bárbara Guieiro <barbaraguieiro@gmail.com>
2023-08-12 19:33:46 -0300

    [feat]inserting comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,47 @@ import styles from "./App.module.css";
 import "./global.css";
 import { Sidebar } from "./components/Sidebar";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/bahguima.png",
+      name: "Barbara Guieiro",
+      role: "FrontEnd Developer",
+    },
+    content: [
+      { type: "paragraphy", content: "Eos facilis similique" },
+
+      {
+        type: "paragraphy",
+        content:
+          " Qui voluptatem exercitationem a voluptas nesciunt ut voluptatem quia ut rerum libero qui voluptas excepturisit itaque obcaecati. Eum sequi quasi ut consequatur repellat qui nihil voluptatem ex corporis provident.",
+      },
+      { type: "link", content: "ignite/react" },
+    ],
+    publishedAt: new Date("20223-08-12 20:00:00"),
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/rstanigher.png",
+      name: "Rafa Stanigher",
+      role: "Ops Team Leader",
+    },
+    content: [
+      { type: "paragraphy", content: "Eos facilis similique" },
+
+      {
+        type: "paragraphy",
+        content:
+          " Qui voluptatem exercitationem a voluptas nesciunt ut voluptatem quia ut rerum libero qui voluptas excepturisit itaque obcaecati. Eum sequi quasi ut consequatur repellat qui nihil voluptatem ex corporis provident.",
+      },
+      { type: "link", content: "ignite/react" },
+    ],
+    publishedAt: new Date("20223-08-11 20:00:00"),
+  },
+];
+
 export default function App() {
   return (
     <div>
@@ -13,10 +54,9 @@ export default function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Bárbara Guieiro"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-          />
+          {posts.map((post) => {
+            return <Post author={post.author} content={post.content} publishedAt={post.publishedAt} />;
+          })}
         </main>
       </div>
     </div>
